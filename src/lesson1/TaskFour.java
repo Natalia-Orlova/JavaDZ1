934020185141e7c2a16d135ca586b31465ed224d
NataliaOrlova <natakarelskaya@yandex.ru>
2022-11-25 16:49:43 +0400

    task4

--- src/lesson1/TaskFour.java
+++ src/lesson1/TaskFour.java
@@ -4,17 +4,18 @@ public class TaskFour {
     public String reverseWords(String s) {
         StringBuilder sb = new StringBuilder();
         int end = s.length() - 1;
-        while (end > 0) {
-            while (s.charAt(end) == ' '){
+        while (end >= 0) {
+            while (end > 0 && s.charAt(end) == ' '){
                 end--;
             }
             int start = end - 1;
-            while (start > 0 && s.charAt(start) != ' ') {
+            while (start >= 0 && s.charAt(start) != ' ') {
                 start--;
             }
             sb.append(s,start + 1, end + 1);
+            sb.append(' ');
+            end = start;
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
-
