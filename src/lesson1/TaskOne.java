630ef17ac6d3ea5ef4fd9eca3af26d5d38d10425
NataliaOrlova <natakarelskaya@yandex.ru>
2022-11-25 12:06:00 +0400

    task1

--- src/lesson1/TaskOne.java
+++ src/lesson1/TaskOne.java
@@ -2,6 +2,21 @@ package lesson1;
 
 public class TaskOne {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int i = m - 1;
+        int j = n - 1;
+        int copyInd = nums1.length - 1;
 
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[copyInd] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[copyInd] = nums2[j];
+                j--;
+            }
+            copyInd--;
+        }
     }
 }
